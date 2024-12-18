22bd353c4f429874db5594e8854ec0efd56a1d8e
RamonFontana <fontanaramon@gmail.com>
2024-12-18 15:34:30 -0300

    add account menu on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Pizza, UtensilsCrossed } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { NavLink } from "./nav-link";
 import { ThemeToggle } from "./theme/theme-toggle";
+import { AccountMenu } from "./account-menu";
 
 export function Header() {
   return (
@@ -21,6 +22,8 @@ export function Header() {
 
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
+          <Separator orientation="vertical" className="h-6" />
+          <AccountMenu />
         </div>
       </div>
     </div>
