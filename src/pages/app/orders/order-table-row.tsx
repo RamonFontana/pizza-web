66ae0af007fc44054dfe736d7798255c95d46666
RamonFontana <fontanaramon@gmail.com>
2024-12-19 14:53:28 -0300

    dialog and order detail

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,22 @@
-import { ArrowRight, Search, X } from 'lucide-react'
-import { TableCell, TableRow } from '../../../components/ui/table'
-import { Button } from '../../../components/ui/button'
-
+import { ArrowRight, Search, X } from "lucide-react";
+import { TableCell, TableRow } from "../../../components/ui/table";
+import { Button } from "../../../components/ui/button";
+import { Dialog, DialogTrigger } from "../../../components/ui/dialog";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         821e78f7asdhdf128h
@@ -37,5 +43,5 @@ export function OrderTableRow() {
         </Button>
       </TableCell>
     </TableRow>
-  )
+  );
 }
